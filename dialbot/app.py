4fc5b1b9fe6c13400fd13a2f35b30b31c78107b2
Stephen Stack <sfstack7500@gmail.com>
2024-02-04 16:43:49 +0800

    api: simple transcribe endpoint with test

--- dialbot/app.py
+++ dialbot/app.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from dialbot.api.transcribe import transcribe_bp
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+app.register_blueprint(transcribe_bp, url_prefix='/api/v1')
+
+if __name__ == '__main__':
+    app.run()
